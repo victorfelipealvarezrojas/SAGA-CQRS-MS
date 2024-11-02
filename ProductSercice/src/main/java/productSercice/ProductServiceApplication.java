1bc7a2225a6b9e146e3e50cd55d0cdde43dc15ba
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2024-11-02 18:14:09 -0300

    add axon framework and command

--- ProductSercice/src/main/java/productSercice/ProductServiceApplication.java
+++ ProductSercice/src/main/java/productSercice/ProductServiceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProductServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
-
 }
--- ProductSercice/src/main/java/productSercice/Rest/ProductsController.java
+++ ProductSercice/src/main/java/productSercice/Rest/ProductsController.java
@@ -1,19 +1,51 @@
 package productSercice.Rest;
 
+import org.axonframework.commandhandling.gateway.CommandGateway;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+import productSercice.command.CreateProductCommand;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductsController {
 
+
+    private final Environment env;
+    private final CommandGateway commandGateway;
+
+    public ProductsController(CommandGateway commandGateway ,Environment env) {
+        this.commandGateway = commandGateway;
+        this.env = env;
+    }
+
     @PostMapping
-    public String createProduct() {
-        return "HTTP Post Handled";
+    public String createProduct(@RequestBody CreateProductRestModel product) {
+
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .productId(UUID.randomUUID().toString())
+                .title(product.getTitle())
+                .price(product.getPrice())
+                .quantity(product.getQuantity())
+                .build();
+
+        String response = "";
+
+        try {
+            response = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return response;
     }
 
     @GetMapping
     public String getProducts() {
-        return "HTTP Get Handled";
+        System.out.println("env = " + env.toString());
+        return "HTTP Get Handled " + env.getProperty("local.server.ports");
     }
 
     @PutMapping
